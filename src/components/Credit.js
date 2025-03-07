def0e41b77aec30975674e55506d24fd80126c6f
Mohamed Aslam Akbar <154296933+Aslam-Akbar@users.noreply.github.com>
2025-03-07 17:08:34 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+      <p>
+        Coded by
+        <a href="https://mohamed-aslam.netlify.app"> Mohamed Aslam A</a>,{" "}
+        <a href="https://github.com/">Open sourced on Github.</a> Hosted on
+        Netlify
+      </p>
+    </div>
+  );
+}
